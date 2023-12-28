e4a00ed35e3e72c37d68b536579b01832a015715
Levent ILHAN <leventilhan.mail@gmail.com>
2023-12-28 14:44:06 +0300

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -8,7 +8,6 @@ import rehypeRaw from "rehype-raw";
 import uuid from 'react-uuid';
 import { isEmpty } from "lodash-es";
 
-import styles from "./Chat.module.css";
 import Azure from "../../assets/Aura.svg";
 
 import {
